7a1fb94b1619e31a8dd6900fe6aea2f0b713509d
yazan nacar <146853884+yazan-nacar@users.noreply.github.com>
2024-09-12 21:07:56 +0300

    upload design

---
+++ js/main.js
@@ -0,0 +1,102 @@
+//scroller settings 
+
+let el = document.querySelector(".scroller");
+let height = document.documentElement.scrollHeight - document.documentElement.clientHeight ;
+
+window.addEventListener("scroll" , () => {
+  let scrollTop = document.documentElement.scrollTop;
+  el.style.width = `${(scrollTop / height) * 100}%`;
+});
+
+
+
+
+
+// The End Of The Year Date
+// 1000 milliseconds = 1 Second
+
+let countDownDate = new Date("Dec 31, 2022 23:59:59").getTime();
+
+
+let counter = setInterval(() => {
+  // Get Date Now
+  let dateNow = new Date().getTime();
+
+  // Find The Date Difference Between Now And Countdown Date
+  let dateDiff = countDownDate - dateNow;
+
+  // Get Time Units
+  // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+  document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+
+  if (dateDiff < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
+
+
+
+
+
+
+let progressSpans = document.querySelectorAll(".the-progress span");
+let section = document.querySelector(".our-skills");
+
+let nums = document.querySelectorAll(".stats .number");
+let statsSection = document.querySelector(".stats");
+let started = false; // Function Started ? No
+
+window.onscroll = function () {
+  // Skills Animate Width
+  if (window.scrollY >= section.offsetTop - 250) {
+    progressSpans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+  }
+  // Stats Increase Number
+  if (window.scrollY >= statsSection.offsetTop) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
+
+
+
+// //up buttton settings 
+// let span = document.querySelector(".up");
+// window.onscroll = function () {
+//   if(this.scrollY >= document.documentElement.clientHeight) {
+//     span.classList.add("show");
+//   }else {
+//     span.classList.remove("show");
+//   }
+// };
+
+// span.onclick = function () {
+//   window.scrollTo({
+//     top: 0,
+//     Behavior : "smooth"
+//   })
+// };
+
+
